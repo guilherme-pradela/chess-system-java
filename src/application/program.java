4858b463da3f035d5a0bd3cd1836dcab3e2551f3
guilherme-pradela <guilhermeprads2005@gmail.com>
2025-11-22 04:29:30 -0300

    peças e tabuleiro

--- src/application/program.java
+++ src/application/program.java
@@ -1,10 +1,12 @@
 package application;
 
+import boardgame.Board;
+
 public class program {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello Word");
+		Board board = new Board(8, 8);
 	}
 
 }
