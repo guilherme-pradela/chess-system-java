2b975b9a3c3f89d59563927282cb00836087f9b8
guilherme-pradela <guilhermeprads2005@gmail.com>
2025-12-01 02:41:03 -0300

    novas Peças

--- src/application/UI.java
+++ src/application/UI.java
@@ -58,7 +58,17 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn :" + chessMatch.getTurn());
+		if (!chessMatch.getCheckMate()) {
 		System.out.println("Waiting player: " + chessMatch.getCurrentplayer());
+		if (chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+			
+		   }
+		}
+		else {
+			System.out.println("CHECKMATE");
+			System.out.println("Winner " + chessMatch.getCurrentplayer());
+		}
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces) {
